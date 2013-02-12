feb850da1adddb6d693b822628fa2bf7523e3f94
Derek Eder <derek.eder@gmail.com>
2013-02-11 22:59:51 -0600

    checking datestamps to do incremental updating, added error handling to FTClient library

--- run_import.php
+++ run_import.php
@@ -27,8 +27,8 @@
   $data_site = ConnectionInfo::$data_site;
   $app_token = ConnectionInfo::$app_tokenn;
   
-  echo "Socrata -> Fusion Tables import by Derek Eder\n\n";
-  echo "app token: $app_token \n";
+  echo "Chicago Vacant Building Finder import by Derek Eder\n\n";
+  echo "Downloading from $data_site... \n";
   
   //Fetch data from Socrata
   $response = NULL;
@@ -67,89 +67,96 @@
     	$ftclient = new FTClientLogin($token);
     	
     	//for clearing out table
-      echo "\nClearing out existing data\n";
-    	$ftclient->query("DELETE FROM $fusionTableId");
-    	
-    	//check how many are in Fusion Tables already
-    	//$ftResponse = $ftclient->query("SELECT Count() FROM $fusionTableId");
-    	//echo "$ftResponse \n";
-    	
-    	//this part is very custom to this particular dataset. If you are using this, here's where the bulk of your work would be: data mapping!
-    	//$ftResponse = $ftclient->query(SQLBuilder::select($fusionTableId, "'DATE RECEIVED'", "", "'DATE RECEIVED' DESC", "1"));
-    	//$ftResponse = trim(str_replace("DATE RECEIVED", "", $ftResponse)); //totally a hack. there's a better way to do this
-    	
-    	//big assumption: socrata will return the data ordered by date. this may not always be the case
-    	//if ($ftResponse != "")
-    	//	$latestInsert = new DateTime(str_replace("DATE RECEIVED", "", $ftResponse));   
-    	//else
-    	//	$latestInsert = new DateTime("1/1/2100"); //if there are no rows, set it to an early date so we import everything
-    	  
-    	//echo "\nLatest FT insert: " . $latestInsert->format('m/d/Y') . "\n";
+      //echo "\nClearing out existing data\n";
+    	//$ftclient->query("DELETE FROM $fusionTableId");
     }
 
   	$insertCount = 0;
-  	$addresss = array();
-      foreach($response["data"] as $row) {
-      	if ($row[9] != "SERVICE REQUEST #") { //first row in this dataset is a duplicate of the column names
-      		
-      		//convert received date in to DateTime format
-      		$receivedDate = new DateTime($row[10]);
-      		
-      		//creating full address column for geocoding
-      		$fullAddress = $row[18] . " " . $row[19] . " " . $row[20] . " " . $row[21] . " chicago IL " . $row[22];
-      		
-      		//if ($receivedDate > $latestInsert) {
-  		    	$insertArray = array(
-  		    	"SERVICE REQUEST #" => $row[9],
-  		    	"DATE RECEIVED" => $receivedDate->format('m/d/Y'),
-  		    	"LOT LOCATION" => $row[11],
-  		    	"DANGEROUS OR HAZARDOUS?" => $row[12], //this column appears to be empty
-  		    	"Dangerous flag" => SQLBuilder::convertToFlag($row[12], "dangerous"),
-  		    	"OPEN OR BOARDED?" => $row[13],
-  		    	"Open flag" => SQLBuilder::convertToFlag($row[13], "open"),
-  		    	"ENTRY POINT" => $row[14],
-  		    	"VACANT OR OCCUPIED?" => $row[15],
-  		    	"Vacant flag" => SQLBuilder::convertToFlag($row[15], "vacant"),
-  		    	"VACANT DUE TO FIRE?" => $row[16],
-  		    	"Fire flag" => SQLBuilder::setEmptyToZero($row[16]), //stored as an int in Socrata
-  		    	"ANY PEOPLE USING PROPERTY?" => $row[17],
-  		    	"In use flag" => SQLBuilder::setEmptyToZero($row[17]), //stored as an int in Socrata
-  		    	"ADDRESS STREET NUMBER" => $row[18],
-  		    	"ADDRESS STREET DIRECTION" => $row[19],
-  		    	"ADDRESS STREET NAME" => $row[20],
-  		    	"ADDRESS STREET SUFFIX" => $row[21],
-  		    	"ZIP CODE" => $row[22],
-  		    	"Full Address" => $fullAddress,
-  		    	"X COORDINATE" => $row[23],
-  		    	"Y COORDINATE" => $row[24],
-  		    	"Ward" => $row[25],
-  		    	"Police District" => $row[26],
-  		    	"Community Area" => $row[27],
-  		    	"LATITUDE" => $row[28],
-  		    	"LONGITUDE" => $row[29],
-  		    	"Location" => "$row[28],$row[29]"
-  		    	);
-  		    
-  		      if (!in_array($fullAddress, $addresss)) {
-              if ($dump_to_csv)
-                fputcsv($fp, $insertArray); //save to CSV
-              else {
-  		    	   $ftclient->query(SQLBuilder::insert($fusionTableId, $insertArray));
-               //FT has an insert throughput limit defined here: 
-               //https://developers.google.com/fusiontables/docs/v1/using#Geo
-               sleep(1); 
-              }
-  		    	  
-              //keep track of addresses inserted
-  		    	  array_push($addresss, $fullAddress);
-  		    	  $insertCount++;
-  		    	  echo "inserted $insertCount so far: " . $receivedDate->format('m/d/Y') . "\n";
-  		      }
-  		    //}
-      	}
-      }
-    }
-    echo "\ninserted $insertCount rows\n";
-    echo "This script ran in " . (time()-$bgtime) . " seconds\n";
-    echo "\nDone.\n";
+    $updateCount = 0;
+  	$unique_addresses = array();
+
+    foreach($response["data"] as $row) {
+  		//convert received date in to DateTime format
+  		$receivedDate = new DateTime($row[10]);
+  		
+  		//creating full address column for geocoding
+  		$fullAddress = $row[18] . " " . $row[19] . " " . $row[20] . " " . $row[21] . " chicago IL " . $row[22];
+  		
+  		//if ($receivedDate > $latestInsert) {
+	    	$insertArray = array(
+	    	"SERVICE REQUEST #" => $row[9],
+	    	"DATE RECEIVED" => $receivedDate->format('m/d/Y'),
+	    	"LOT LOCATION" => $row[11],
+	    	"DANGEROUS OR HAZARDOUS?" => $row[12], //this column appears to be empty
+	    	"Dangerous flag" => SQLBuilder::convertToFlag($row[12], "dangerous"),
+	    	"OPEN OR BOARDED?" => $row[13],
+	    	"Open flag" => SQLBuilder::convertToFlag($row[13], "open"),
+	    	"ENTRY POINT" => $row[14],
+	    	"VACANT OR OCCUPIED?" => $row[15],
+	    	"Vacant flag" => SQLBuilder::convertToFlag($row[15], "vacant"),
+	    	"VACANT DUE TO FIRE?" => $row[16],
+	    	"Fire flag" => SQLBuilder::setEmptyToZero($row[16]), //stored as an int in Socrata
+	    	"ANY PEOPLE USING PROPERTY?" => $row[17],
+	    	"In use flag" => SQLBuilder::setEmptyToZero($row[17]), //stored as an int in Socrata
+	    	"ADDRESS STREET NUMBER" => $row[18],
+	    	"ADDRESS STREET DIRECTION" => $row[19],
+	    	"ADDRESS STREET NAME" => $row[20],
+	    	"ADDRESS STREET SUFFIX" => $row[21],
+	    	"ZIP CODE" => $row[22],
+	    	"Full Address" => $fullAddress,
+	    	"X COORDINATE" => $row[23],
+	    	"Y COORDINATE" => $row[24],
+	    	"Ward" => $row[25],
+	    	"Police District" => $row[26],
+	    	"Community Area" => $row[27],
+	    	"LATITUDE" => $row[28],
+	    	"LONGITUDE" => $row[29],
+	    	"Location" => "$row[28],$row[29]"
+	    	);
+	    
+	      if (!in_array($fullAddress, $unique_addresses)) {
+          if ($dump_to_csv) {
+            fputcsv($fp, $insertArray); //save to CSV
+            //keep track of addresses inserted
+            array_push($unique_addresses, $fullAddress);
+            $insertCount++;
+          }
+          else {
+            $row_received_date = fetch_by_address("'DATE RECEIVED'", $fullAddress, $ftclient, $fusionTableId);
+            if ($row_received_date == NULL) {
+              $ftclient->query(SQLBuilder::insert($fusionTableId, $insertArray));
+
+              //FT has an insert throughput limit of 0.5 qps defined here: 
+              //https://developers.google.com/fusiontables/docs/v1/using#Geo
+              sleep(1); 
+              $insertCount++;
+              echo "inserted $insertCount so far: " . $fullAddress . "\n";
+            }
+            else if (new DateTime($row_received_date) < $receivedDate) {
+              $updated_date = new DateTime($row_received_date);
+              $row_id = fetch_by_address('ROWID', $fullAddress, $ftclient, $fusionTableId);
+              $ftclient->query(SQLBuilder::update($fusionTableId, $insertArray, $row_id));
+              $updateCount++;
+              echo "updating $fullAddress " . $updated_date->format('m/d/Y') . "\n";;
+              echo "updated $updateCount so far: " . $fullAddress . "\n";
+            }
+          }
+	      }
+	    //}
+  	}
+  }
+  echo "\ninserted $insertCount rows\n";
+  echo "\nupdated $updateCount rows\n";
+  echo "This script ran in " . (time()-$bgtime) . " seconds\n";
+  echo "\nDone.\n";
+
+  function fetch_by_address($column, $fullAddress, $ftclient, $fusionTableId) {
+    $response = $ftclient->query(SQLBuilder::select($fusionTableId, "$column", "'Full Address' = '$fullAddress'"));
+
+    //echo var_dump($response);
+    if (count($response) > 1)
+      return $response[1][0];
+    else
+      return NULL;
+  }
 ?>
